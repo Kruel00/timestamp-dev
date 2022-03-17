35967ccf8e265c19a13a679996b3c6d87897acdb
Javier Castorena <javier.castorena@reconext.com>
2022-03-17 15:44:26 -0700

    raw timestamp

--- timestamp.c
+++ timestamp.c
@@ -21,7 +21,7 @@
 #define VN(packet)   (uint8_t) ((packet.li_vn_mode & 0x38) >> 3) 
 #define MODE(packet) (uint8_t) ((packet.li_vn_mode & 0x07) >> 0) 
 
-long int timestamp;
+long int timeRcv;
 ntp_server_t timestam;
 ntp_packet packet = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 int sockfd, n; 
@@ -36,16 +36,10 @@ void *timeout_server()
   memset( &packet, 0, sizeof( ntp_packet ) );
   *( ( char * ) &packet + 0 ) = 0x1b;
 
-  printf("creando soket...\n");
+  
   sockfd = socket( AF_INET, SOCK_DGRAM, IPPROTO_UDP ); // Create a UDP socket.
   if (sockfd < 0)
         printf("error al crear el soket\n");
-
-  printf("conectando al server %s\n",PXE_SERVER_ADRESS );
-  char *IPbuffer;
-  IPbuffer = inet_ntoa(*((struct in_addr*)
-                           server->h_addr_list[0]));
-  printf("direccion %s\n",IPbuffer);
 
   bzero( ( char* ) &serv_addr, sizeof( serv_addr ) );
   serv_addr.sin_family = AF_INET;
@@ -56,19 +50,11 @@ void *timeout_server()
   if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
       printf("error al conectar");
 
-  printf("enviando paquete...\n");
   n = write( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
-  printf("Paquete enviado...%i\n",n);
-
   n = read( sockfd, ( char* ) &packet, sizeof( ntp_packet ) );
-
-  printf("paquete recivido...\n");
   packet.txTm_s = ntohl( packet.txTm_s ); // Time-stamp seconds.
 
-  timestamp = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
-
-  printf("asjdkhasjdh %li\n",timestamp);
-
+  timeRcv = ( time_t ) ( packet.txTm_s - NTP_TIMESTAMP_DELTA );
 }
   
 
@@ -82,7 +68,7 @@ time_t get_pxe_server_timestamp(){
   pthread_create(&thread_id,NULL,*timeout_server,NULL);  
   sleep(3);
   pthread_cancel(thread_id);
-  return timestamp;
+  return timeRcv;
 }
 
 
